e99ea1f3a41caf953fe69a3918cc0e6665014374
leowang <leowang9863@gmail.com>
2022-01-02 20:01:38 -0500

    updated index page

--- components/Hero.js
+++ components/Hero.js
@@ -2,22 +2,22 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center mb-20 mt-20">
+    <div className="flex flex-col justify-center items-center mb-10 md:mb-20 mt-20">
       <img className="max-w-full w-40" src="avatar.png" alt="" />
       <div className="flex flex-col justify-center items-center mt-5 md:mt-10 ">
         <p className="text-xl text-gray-500 tracking-wider font-bold dark:text-gray-400">
           HI I&apos;M LEO{" "}
         </p>
-        <h1 className="text-4xl font-medium  dark:text-white">
+        <h1 className="text-4xl md:text-5xl font-medium  dark:text-white mt-3">
           Software Engineer / Web Designer
         </h1>
-        <p className="text-2xl font-medium mt-2 dark:text-white">
+        {/* <p className="text-2xl font-medium mt-2 dark:text-white">
           Also a Filmmaker / Technology Enthusiast / Public Speaker
-        </p>
+        </p> */}
         {/* <h1 className="text-4xl font-medium mt-2 dark:text-white">
           Second-year Master&apos;s at NYU
         </h1> */}
-        <p className="text-xl text-gray-500 mt-2 dark:text-gray-400">
+        <p className="text-2xl text-gray-500 mt-3 dark:text-gray-400">
           I specialize in developing minimal web applications that solves real
           world problems{" "}
         </p>
